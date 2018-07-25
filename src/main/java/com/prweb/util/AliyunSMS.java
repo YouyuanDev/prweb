93dafb77e4a51e9bd9f04397fde3482bc39e4c09
ketewang <ketewang@youyuantech.com>
2018-07-25 12:41:53 +0800

    修改模版

--- src/main/java/com/prweb/util/AliyunSMS.java
+++ src/main/java/com/prweb/util/AliyunSMS.java
@@ -105,7 +105,7 @@ public class AliyunSMS {
         //必填:短信签名-可在短信控制台中找到
         request.setSignName(signature);
         //必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode("SMS_140525106");
+        request.setTemplateCode("SMS_140550198");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
 
 
